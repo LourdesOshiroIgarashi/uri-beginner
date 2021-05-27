6fe70a6adfd197399414e2bf2c76b54d41838ecb
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-05-27 15:07:50 -0400

    Create 1006.py

---
+++ python/1006.py
@@ -0,0 +1,11 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+notaA = A * 2
+notaB = B * 3
+notaC = C * 5
+
+final = (notaA + notaB + notaC) / 10
+
+print("MEDIA = {0:.1f}".format(final))
