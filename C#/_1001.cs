f8c0bc8aaa1c1ca50a3ea8bad44883d91ff2427f
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-08-18 11:25:06 -0400

    Create _1001.cs

---
+++ C#/_1001.cs
@@ -0,0 +1,16 @@
+using System;
+namespace _1001
+{
+    class _1001
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int soma = a + b;
+
+            Console.WriteLine("X = {0}", soma);
+        }
+    }
+}
