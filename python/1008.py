545f08661c90f193247654e617f411e7dcd8dd08
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-08-18 21:22:36 -0400

    Create 1008.py

---
+++ python/1008.py
@@ -0,0 +1,8 @@
+numero = int(input())
+xhoras = int(input())
+porhora = float(input())
+
+salario = (xhoras * porhora)
+
+print("NUMBER = {0}".format(numero))
+print("SALARY = U$ {0:.2f}".format(salario))
